47908cc35f1ab0512f9c20419d61e50c202d64f2
notduyly <162235323+notduyly@users.noreply.github.com>
2024-09-10 16:10:59 -0700

    Game Finished
    Finished building the game and is fully functional. Yay!

--- script.js
+++ script.js
@@ -1,18 +1,148 @@
 // Define HTML elements
 const board = document.getElementById("game-board");
+const instructionText = document.getElementById("instruction-text");
+const logo = document.getElementById("logo");
+const score = document.getElementById("score");
+const highScoreText = document.getElementById("highScore");
 
-const gridSize = 20;
+const gridSize = 19;
 let snake = [{x: 10, y: 10}];
 let food = generateFood();
-let direction = "right"
+let highScore = 0;
+let direction = "right";
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
 
-//draw game map, snake, food
+
+//function of the game
+function move() {
+  const head = {...snake[0]}
+  switch(direction) {
+    case "right":
+      head.x++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "up":
+      head.y--;
+      break;
+    case "down":
+      head.y++;
+      break;
+  }
+  snake.unshift(head);
+
+  if (head.x === food.x && head.y === food.y){
+    food = generateFood();
+    increaseSpeed();
+    clearInterval(gameInterval);
+    gameInterval =  setInterval(() => {
+      move();
+      checkCollision();
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+function handleKeyPress(event){
+  if ((!gameStarted && event.code === "Space") || 
+      (!gameStarted && event.key === " ")){
+    startGame()
+  } else {
+    switch (event.key) {
+      case "ArrowUp":
+        direction = "up";
+        break;
+      case "ArrowDown":
+        direction = "down";
+        break;
+      case "ArrowLeft":
+        direction = "left";
+        break;
+      case "ArrowRight":
+        direction = "right";
+        break;
+    }
+  }
+}
+document.addEventListener("keydown", handleKeyPress);
+
+function increaseSpeed() {
+  console.log(gameSpeedDelay);
+  if (gameSpeedDelay > 150){
+    gameSpeedDelay -= 5;
+  } else if (gameSpeedDelay > 100){
+    gameSpeedDelay -= 3;
+  } else if (gameSpeedDelay > 50){
+    gameSpeedDelay -= 2;
+  } else {
+    gameSpeedDelay -= 1;
+  } 
+}
+function checkCollision() {
+  const head = snake[0];
+  if (head.x < 1 || head.x > gridSize
+    || head.y < 1 || head.y > gridSize) {
+      resetGame();
+  }
+  for (let i = 1; i < snake.length; i++){
+    if (head.x === snake[i].x && head.y === snake[i].y){
+      resetGame();
+    }
+  }
+}
+
+//start/stop/reset game
+function startGame() {
+  gameStarted = true;
+  instructionText.style.display = "none";
+  logo.style.display = "none";
+  gameInterval = setInterval(() => {
+    move();
+    checkCollision();
+    draw();
+  }, gameSpeedDelay);
+}
+function stopGame(){
+  clearInterval(gameInterval);
+  gameStarted = false;
+  instructionText.style.display = "block";
+  logo.style.display = "block";
+}
+function resetGame() {
+  updateHighScore();
+  stopGame();
+  snake = [{x: 10, y: 10}];
+  food = generateFood();
+  direction = "right";
+  gameSpeedDelay = 200;
+  updateScore();
+}
+
+//updating scores
+function updateScore() {
+  const currentScore = snake.length - 1;
+  score.textContent = currentScore.toString().padStart(3, "0");
+}
+function updateHighScore() {
+  const currentScore = snake.length - 1;
+  if (currentScore > highScore){
+    highScore = currentScore;
+    highScoreText.textContent = "High score: " + highScore.toString().padStart(3, "0");
+  }
+  highScoreText.style.display = "block";
+}
+
+//'drawing' the game
 function draw() {
   board.innerHTML = "";
   drawSnake();
   drawFood();
+  updateScore();
 }
-
 function drawSnake() {
   snake.forEach((segment) => {
     const snakeElement = createGameElement("div", "snake");
@@ -20,27 +150,24 @@ function drawSnake() {
     board.appendChild(snakeElement);
   });
 }
-
 function drawFood() {
-  const foodElement = createGameElement("div", "food");
-  setPosition(foodElement, food);
-  board.appendChild(foodElement);
+  if (gameStarted){
+    const foodElement = createGameElement("div", "food");
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+  }
 }
-
 function createGameElement(tag, className) {
   const element = document.createElement(tag);
   element.className = className;
   return element;
 }
-
 function setPosition(element, position) {
   element.style.gridColumn = position.x;
   element.style.gridRow = position.y;
 }
-
 function generateFood() {
   const x = Math.round(Math.random() * gridSize) + 1;
   const y = Math.round(Math.random() * gridSize) + 1;
   return {x, y};
 }
-
